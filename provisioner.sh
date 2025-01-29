1d5359e8d553f67c9bcf93bdbb60f734f3a44b53
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-01-29 12:52:46 -0600

    Update provisioner.sh

--- provisioner.sh
+++ provisioner.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
-echo "<h1>Hello, Packer AMI</h1>" > /var/www/html/index.html
+
+# Ensure the correct ownership and permissions
+sudo chown -R apache:apache /var/www/html
+sudo chmod -R 755 /var/www/html
+
+# Now write the index.html file
+echo "<h1>Welcome to My Web Server</h1>" | sudo tee /var/www/html/index.html
