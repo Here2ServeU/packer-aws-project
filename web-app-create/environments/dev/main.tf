a64477c7f829963bb26e345830d60f357e680246
Emmanuel <revnaweji@gmail.com>
2025-01-31 14:04:44 -0600

    Updated scripts

--- web-app-create/environments/dev/main.tf
+++ web-app-create/environments/dev/main.tf
@@ -43,7 +43,7 @@ module "security_group" {
 module "ec2" {
   source           = "../../modules/ec2"
   subnet_id = data.aws_subnet.default_subnet.id 
-  ami_id           = "ami-0e1bed4f06a3b463d"
+  ami_id           = "ami-0ba81b1086ea758e7"
   instance_type    = "t2.micro"
   key_name         = aws_key_pair.web_app_key_pair.key_name
   security_group_id = module.security_group.security_group_id
